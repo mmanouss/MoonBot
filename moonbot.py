d3bc61e4c2092430a7202b092e3869492b76dc67
mmanouss <mmanousselis@gmail.com>
2022-10-29 15:55:09 -0700

    Added function that processes files into lists

--- moonbot.py
+++ moonbot.py
@@ -1,11 +1,26 @@
 import os
 import discord
+import random
 from dotenv import load_dotenv
 
 load_dotenv('BOT_TOKEN.env')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 client = discord.Client() 
+
+def fileToList(fileName: str) -> list:
+    """Converts a given file into a list"""
+    fileNamefile = open(fileName, "r")
+    fileList = fileNamefile.readlines()
+    return fileList
+
+moonfacts = fileToList("moonfacts.txt")
+moonimgs = fileToList("moonimgs.txt")
+moongifs = fileToList("moongifs.txt")
+moonvideos = fileToList("moonvideos.txt")
+
+moonmedia = moonimgs + moongifs + moonvideos
+random.shuffle(moonmedia)
 
 @client.event
 async def on_ready():
