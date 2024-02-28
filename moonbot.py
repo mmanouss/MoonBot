833484cd82067fedfb87096ec976432ea5f9b6d4
mmanouss <mmanousselis@gmail.com>
2024-02-28 01:12:15 -0800

    Added keyword search functionality in moon-fact command

--- moonbot.py
+++ moonbot.py
@@ -40,11 +40,23 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='moon enjoyers :)'))
     print(f'Logged in as {bot.user}')
                
-@bot.slash_command(name="moon-fact", description="Send a random moon fact.")
-async def moon_fact(ctx):
+@bot.slash_command(name="moon-fact", description="Send a random moon fact, with optional keyword.")
+async def moon_fact(ctx, keyword: str = None):
     await ctx.defer()
+    # Filter moonfacts based on the provided keyword
+    filtered_facts = []
+    if keyword:
+        filtered_facts = [fact for fact in moonfacts if keyword.lower() in fact.lower()]
+    if not keyword or not filtered_facts:
+        filtered_facts = moonfacts
+
+    random_fact = random.choice(filtered_facts)
     embed = discord.Embed(title="Moon Fact!", color=0x36393e)
-    embed.add_field(name="⁺₊ Did you know? ⁺₊", value=randomIndex(moonfacts), inline=False)
+    if filtered_facts == moonfacts:
+        no_kword = "Unfortunately, no moon facts were found containing the provided keyword '" + keyword + "'.\n\n" + "⁺₊ Have a random moon fact anyway! ⁺₊"
+        embed.add_field(name=no_kword, value=random_fact, inline=False)
+    else:
+        embed.add_field(name="⁺₊ Did you know? ⁺₊", value=random_fact, inline=False)
     await ctx.respond(embed=embed)
 
 @bot.slash_command(name="moon-image", description="Send a random moon image.")
@@ -77,7 +89,7 @@ async def moon_video(ctx):
 async def help_command(ctx):
     await ctx.defer()
     embed = discord.Embed(title="☁︎☾☁︎ Moon Bot Commands ☁︎☾☁︎", color=0x36393e)
-    embed.add_field(name="moon-fact", value="Send a random moon fact.", inline=False)
+    embed.add_field(name="moon-fact", value="Send a random moon fact, with optional keyword.", inline=False)
     embed.add_field(name="moon-media", value="Send random moon media.", inline=False)
     embed.add_field(name="moon-image", value="Send a random moon image.", inline=False)
     embed.add_field(name="moon-gif", value="Send a random moon gif.", inline=False)
